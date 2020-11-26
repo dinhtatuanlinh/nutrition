1d6fe403ce0b1a840da827a84414ca9f8646f4d7
Dinh Ta Tuan Linh <dinhtatuanlinh@gmail.com>
2020-11-26 17:09:05 +0700

    fix losing main.css

--- header-top.php
+++ header-top.php
@@ -5,12 +5,14 @@
     // print_r($webElements);
 ?>
 <header id="header" style="background-image: url(''); background-repeat: no-repeat; background-size: cover;">
-                    <a href="<?php echo home_url(); ?>" ><img class="dttllogo" src="<?php echo $aboutData['logo']; ?>" alt=""></a>
-                    <ul class="icons">
-                        <li><a href="<?php echo $aboutData['youtube']; ?>" class="icon brands fa-youtube dttlyoutube"><span class="label">youtube</span></a></li>
-                        <li><a href="<?php echo $aboutData['facebook']; ?>" class="icon brands fa-facebook-f dttlfacebook"><span class="label">Facebook</span></a></li>
-                        <!-- <li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+    <a href="<?php echo home_url(); ?>"><img class="dttllogo" src="<?php echo $aboutData['logo']; ?>" alt=""></a>
+    <ul class="icons">
+        <li><a href="<?php echo $aboutData['youtube']; ?>" class="icon brands fa-youtube dttlyoutube"><span
+                    class="label">youtube</span></a></li>
+        <li><a href="<?php echo $aboutData['facebook']; ?>" class="icon brands fa-facebook-f dttlfacebook"><span
+                    class="label">Facebook</span></a></li>
+        <!-- <li><a href="#" class="icon brands fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
                         <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
                         <li><a href="#" class="icon brands fa-medium-m"><span class="label">Medium</span></a></li> -->
-                    </ul>
-                </header>
+    </ul>
+</header>
